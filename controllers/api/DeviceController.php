159015972654c61d35db7be860d8aeebf9fc00b5
Cyruswen <1321336357@qq.com>
2019-05-18 13:26:25 +0800

    fix bug

--- controllers/api/DeviceController.php
+++ controllers/api/DeviceController.php
@@ -196,4 +196,25 @@ class DeviceController extends GraduationProjectBaseController
             'result' => $resultData,
         ];
     }
+
+    public function actionMinTime()
+    {
+        //校验参数
+        $filed = ['cost'];
+        $userService = new UserService();
+        $result = $userService->checkParams($filed, $this->params);
+        if (!$result) {
+            $this->response = [
+                'code' => BsEnum::PARAMS_ERROR_CODE,
+                'reason' => BsEnum::$codeMap[BsEnum::PARAMS_ERROR_CODE],
+            ];
+            return;
+        }
+
+        $deviceService = new DeviceService();
+        $resultData = $deviceService->getMaxCostData($this->params['cost'], "d_trscws", "d_tcscws");
+        $this->response = [
+            'result' => $resultData,
+        ];
+    }
 }
