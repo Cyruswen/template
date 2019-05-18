dfb8e50e23328689fbeac706454a02dfacb4e5bd
Cyruswen <1321336357@qq.com>
2019-05-18 11:03:52 +0800

    fix bug

--- domain/DeviceService.php
+++ domain/DeviceService.php
@@ -172,7 +172,7 @@ class DeviceService
         $userModel = new UserModel();
         $data = ["d_m", "d_c", "d_a", "d_n", "d_vx", "d_vy", "d_ax", "d_ay", "d_vz", "d_trscws", "d_ecscws", "d_tcscws", "d_cr","d_cs"];
         $orderData = $userModel->getOrderData($table, $data, $maxCost, "d_trscws");
-        return $orderData[0];
+        return end($orderData);
     }
 
     public function formTemperature($temperature)
