011006d844f26a26b19ae2c43e9768a9e8b10bb3
Cyruswen <1321336357@qq.com>
2019-06-02 12:08:28 +0800

    fix bug

--- models/UserModel.php
+++ models/UserModel.php
@@ -120,7 +120,7 @@ class UserModel extends ActiveRecord
     public function queryTemperature($tableName, $data, $did, $time, $interval)
     {
         $update_time = intval($time) - $interval;
-        $selectData = (new Query())->select($data)->from($tableName)->where("update_time " . ">'" . $time . "'" . " and update_time " . "< '" . $update_time . "'" . " and did = " . $did)->all();
+        $selectData = (new Query())->select($data)->from($tableName)->where("update_time " . "<'" . $time . "'" . " and update_time " . "> '" . $update_time . "'" . " and did = " . $did)->all();
         return $selectData;
     }
 }
